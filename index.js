773d30873f927e3b15836faeecfefa44037bf4ec
Toan Pham <pqt2p1@gmail.com>
2022-01-15 18:34:11 +0700

    add gitignore, node version

--- index.js
+++ index.js
@@ -64,6 +64,12 @@ async function broadcastBitcoinPrice() {
   }
 }
 
-server.listen(process.env.PORT || 3000, () => {
+let port = process.env.PORT
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+server.listen(port, () => {
   console.log("Server is running on port 3000");
 });
